99ed70ff7b19e2a7524c6e5cb60743093b93d179
jeremycolfer27 <ii21038@bristol.ac.uk>
2023-11-27 17:12:32 +0000

    ported OG portfolio into react

--- react-porftfolio/src/App.js
+++ react-porftfolio/src/App.js
@@ -1,24 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import './styles.css'
 
+import { Gradient } from 'whatamesh'
 function App() {
+  const gradient = new Gradient()
+  gradient.initGradient('#gradient-canvas')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* // <!-------------- Background Gradient --------------> */}
+    <div className="gradient-container">
+        {/* <!-- <div id="box">     --> */}
+        <canvas id="gradient-canvas" data-transition-in/>
+       
     </div>
+    {/* <script src="Gradient.js"></script> */}
+    {/* <script>
+        //import { Gradient } from './Gradient.js'
+
+        // Create your instance
+        const gradient = new Gradient()
+
+        // Call `initGradient` with the selector to your canvas
+        gradient.initGradient('#gradient-canvas')
+    </script> */}
+   {/* <!-------------- Background Gradient --------------> */}
+
+
+   <div className="noise"></div>
+
+  <div className="grid-container">
+
+    <div className="name">
+      <h1>
+        <span className="liam">LIAM K</span>
+        <span className="jones"> JONES</span>
+      </h1>
+    </div>
+
+    <div className="bio">
+      <p>I am currently in my third year pursuing a Computer Science with Innovation degree (MEng), where I've gained valuable experience integrating core computer science skills in diverse, multi-disciplinary Agile projects.</p>
+    </div>
+
+    <div className="buttons">
+      <button type="button">About</button>
+      <button type="button">Projects</button>
+      <button type="button">Contact</button>
+    </div>
+
+  
+ 
+</div>
+</>
+
   );
 }
 
