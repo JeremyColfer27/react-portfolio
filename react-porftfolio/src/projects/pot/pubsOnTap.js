5e87e0962374903273670f357b45dcc0e4be5329
Liam Jones <um21605@bristol.ac.uk>
2024-05-16 20:56:00 +0100

    changed something

--- react-porftfolio/src/projects/pot/pubsOnTap.js
+++ react-porftfolio/src/projects/pot/pubsOnTap.js
@@ -41,30 +41,13 @@ function TemplatePage(props){
                 </button>
             </div>
 
-            <div className="expanded-project-body">
+            <div className={styles["expanded-project-body"]}>
+                <div className={styles["debug-item"]}>Item 1</div>
+                <div className={styles["debug-item"]}>Item 2</div>
+                <div className={styles["debug-item"]}>Item 3</div>
+                <div className={styles["debug-item"]}>Item 4</div>
 
-                <div className={modualiseClassNames(["project-headline"])}>
-                    {/* <h1 className={modualiseClassNames(["project.title"])}>Pubs on Tap</h1> */}
-                    {/* <img src="./logo192.png" alt="project image" />   */}
-                    <img src={header_img} height="9px" width="16px" alt="" />
-                </div>
-
-                <p className={modualiseClassNames(["project-description"])}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur odit repellat voluptatibus. Corporis accusantium perferendis id, laudantium illo cupiditate cum esse aperiam, dolore non molestias accusamus veritatis. Voluptate soluta alias cumque veniam quo natus, in eum deserunt eius enim minus quibusdam recusandae neque, adipisci harum impedit illum distinctio commodi dolore! Commodi iste ex dolorum porro libero, nesciunt quo cumque ut!</p>
-
-                <div className={modualiseClassNames(["project-main-content"])}>
-                <div className={modualiseClassNames(["project-section"])}>
-                    <h2>Project section 1</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nihil aut eos quaerat sed deserunt molestiae alias tenetur obcaecati temporibus voluptates officiis id, ipsa eveniet sint odit enim earum molestias, eaque porro, saepe reprehenderit quibusdam quae. Doloremque asperiores repellendus corrupti.</p>
-                </div>
-                <div className={modualiseClassNames(["project-section"])}>
-                    <h2>Project section 2</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nihil aut eos quaerat sed deserunt molestiae alias tenetur obcaecati temporibus voluptates officiis id, ipsa eveniet sint odit enim earum molestias, eaque porro, saepe reprehenderit quibusdam quae. Doloremque asperiores repellendus corrupti.</p>
-                </div>
-                <div className={modualiseClassNames(["project-section"])}>
-                    <h2>Project section 3</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nihil aut eos quaerat sed deserunt molestiae alias tenetur obcaecati temporibus voluptates officiis id, ipsa eveniet sint odit enim earum molestias, eaque porro, saepe reprehenderit quibusdam quae. Doloremque asperiores repellendus corrupti.</p>
-                </div>
-                </div>
+                
 
             </div>
         </div>
